2e4b66d5d061ec75d4dc10b637f42e22384c7319
Nikita Melnikov <nikmel2803@gmail.com>
2018-10-10 22:04:18 +0300

    lab_05 add 1-6 tasks

--- lab_05/lab_05/settings.py
+++ lab_05/lab_05/settings.py
@@ -52,7 +52,9 @@ ROOT_URLCONF = 'lab_05.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ lab_05/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse("Hello, world!")
+
+
+def indexRender(request):
+    return render(request, 'index.html', {})
